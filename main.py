b73a4e416876913e0698a598a079cd4b713c9477
Nacho <isanccal@myuax.com>
2024-09-20 18:58:07 +0200

    aa

---
+++ main.py
@@ -0,0 +1,7 @@
+from subclases.coche import Coche
+
+# main.py
+
+if __name__ == "__main__":
+    mi_coche = Coche("azul",4,150,1200)
+    print(mi_coche)
---
+++ subclases/bicicleta.py
@@ -0,0 +1,6 @@
+class Bicicleta(Vehiculo):
+    def __init__(self,color,ruedas,tipo):
+        Vehiculo.__init__(self,color,ruedas)
+        self.tipo=tipo
+    def __str__(self):
+        return Vehiculo.__str__(self)+" {}".format(self.tipo)
